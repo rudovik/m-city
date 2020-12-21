ff5e60b8cadea3b8a13c560e42ffb932e1adda0e
rudovik <giguni@ukr.net>
2020-12-21 15:09:15 +0200

    Feature section completed.

--- src/components/Home/Featured/Stripes.js
+++ src/components/Home/Featured/Stripes.js
@@ -9,55 +9,56 @@ const stripes = [
   { background: '#98c5e9', left: 600, rotate: 25, top: -498, delay: 400 },
 ]
 
-const Stripes = () => {
-  const showStripes = () =>
-    stripes.map((stripe, i) => (
-      <Animate
-        key={i}
-        show={true}
-        start={{
-          background: '#ffffff',
-          opacity: 0,
-          left: 0,
-          rotate: 0,
-          top: 0,
-        }}
-        enter={{
-          background: [stripe.background],
-          opacity: [1],
-          left: [stripe.left],
-          rotate: [stripe.rotate],
-          top: [stripe.top],
-          timing: { delay: stripe.delay, duration: 200, ease: easePolyOut },
-          events: {
-            end() {
-              // console.log('Animation finished')
-            },
+const showStripes = () =>
+  stripes.map((stripe, i) => (
+    <Animate
+      key={i}
+      show={true}
+      start={{
+        background: '#ffffff',
+        opacity: 0,
+        left: 0,
+        rotate: 0,
+        top: 0,
+      }}
+      enter={{
+        background: [stripe.background],
+        opacity: [1],
+        left: [stripe.left],
+        rotate: [stripe.rotate],
+        top: [stripe.top],
+        timing: { delay: stripe.delay, duration: 200, ease: easePolyOut },
+        events: {
+          end() {
+            // console.log('Animation finished')
           },
-        }}
-        interpolation={(begValue, endValue, attr) => {
-          // pass as prop
-          if (attr === 'transform') {
-            return interpolateTransformSvg(begValue, endValue)
-          }
+        },
+      }}
+      interpolation={(begValue, endValue, attr) => {
+        // pass as prop
+        if (attr === 'transform') {
+          return interpolateTransformSvg(begValue, endValue)
+        }
 
-          return interpolate(begValue, endValue)
-        }}
-      >
-        {({ opacity, left, background, rotate, top }) => {
-          return (
-            <div
-              className='stripe'
-              style={{
-                opacity,
-                background,
-                transform: `rotate(${rotate}deg) translate(${left}px,${top}px)`,
-              }}
-            ></div>
-          )
-        }}
-      </Animate>
-    ))
+        return interpolate(begValue, endValue)
+      }}
+    >
+      {({ opacity, left, background, rotate, top }) => {
+        return (
+          <div
+            className='stripe'
+            style={{
+              opacity,
+              background,
+              transform: `rotate(${rotate}deg) translate(${left}px,${top}px)`,
+            }}
+          ></div>
+        )
+      }}
+    </Animate>
+  ))
+
+const Stripes = () => {
   return <div className='featured_stripes'>{showStripes()}</div>
 }
 
